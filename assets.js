25054f9fc8ce46a7de9119d1ffdaa111691aae12
Akechi Satori <i@menhera.io>
2019-05-19 21:24:22 +0800

    init

---
+++ assets.js
@@ -0,0 +1,61 @@
+module.exports = {
+    plugin_yml_code: `name: {{project_name}}
+main: {{main_method}}
+version: 1.00`,
+
+    main_method_code: `package {{package_name}};
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class {{main_method_name}} extends JavaPlugin {
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
+`, 
+    pom_xml_file: `<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xsi:schemaLocation="http://maven.apache.org/POM/4.0.0http://maven.apache.org/xsd/maven-4.0.0.xsd">
+<modelVersion>4.0.0</modelVersion>
+<groupId>{{package_name}}</groupId>
+<artifactId>{{project_name}}</artifactId>
+<version>1.0.0-SNAPSHOT</version>
+<packaging>jar</packaging>
+<properties>
+    <file.encoding>UTF-8</file.encoding>
+</properties>
+<repositories>
+    <repository>
+        <id>papermc</id>
+        <url>https://papermc.io/repo/repository/maven-public/</url>
+    </repository>
+</repositories>
+<dependencies>
+    <dependency>
+        <groupId>com.destroystokyo.paper</groupId>
+        <artifactId>paper-api</artifactId>
+        <version>{{paperspigot_version}}</version>
+    </dependency>
+</dependencies>
+<build>
+    <defaultGoal>install</defaultGoal>
+    <plugins>
+        <plugin>
+            <groupId>org.apache.maven.plugins</groupId>
+            <artifactId>maven-compiler-plugin</artifactId>
+            <version>3.1</version>
+            <configuration>
+                <source>1.8</source>
+                <target>1.8</target>
+                <encoding>utf-8</encoding>
+            </configuration>
+        </plugin>
+    </plugins>
+</build>
+</project>`
+}
---
+++ index.js
@@ -0,0 +1,107 @@
+#!/usr/bin/env node
+const ArgumentParser = require('argparse').ArgumentParser;
+const argparse = require('argparse');
+const assets = require('./assets');
+const xml2json = require('xml2js');
+const readline = require('readline-sync');
+const axios = require('axios');
+const fs = require('fs');
+
+const paperspigot_maven_metadata_url = "https://papermc.io/repo/repository/maven-public/com/destroystokyo/paper/paper-api/maven-metadata.xml";
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
+var parser = new ArgumentParser({
+    version: '0.0.1',
+    addHelp: true,
+    description: 'Minecraft Plugin Skeleton Builder'
+});
+const args = parser.parseArgs();
+const deleteall = (path) => {
+	var files = [];
+	if(fs.existsSync(path)) {
+		files = fs.readdirSync(path);
+		files.forEach(function(file, index) {
+			var curPath = path + "/" + file;
+			if(fs.statSync(curPath).isDirectory()) {
+				deleteall(curPath);
+			} else {
+				fs.unlinkSync(curPath);
+			}
+		});
+		fs.rmdirSync(path);
+	}
+};
+const core = () => {
+    const project_name = readline.question('project name: ')
+    if (fs.existsSync(project_name)) {
+        const bool = readline.question(`Project [${project_name}] Exists, Override it ? (Y/N) : `)
+        let answer = bool.toUpperCase().toString();
+        if (answer === 'Y' || answer === '') {
+            deleteall(project_name);
+        } else {
+            process.exit();
+        }
+    }
+    const package_name = readline.question('package name: ');
+    const main = readline.question('main method: (main): ');
+    var main_method = '';
+    if (main.length == 0) {
+        main_method = 'main'
+    } else {
+        main_method = main;
+    }
+    try {
+        const src_folder = package_name.replaceAll('.','/');
+        const source_folder_full_path = `${project_name}/src/${src_folder}`;
+        fs.mkdirSync(`${source_folder_full_path}`, {
+            recursive: true
+        });
+        axios.get(paperspigot_maven_metadata_url).then((res) => {
+            xml2json.parseString(res.data, (error, result) => {
+                var select_version;
+                const version = result.metadata.versioning[0];
+                const latest_version = version['latest'][0];
+                console.log("Version List: ");
+                const version_list = version['versions'][0]['version'];
+                version_list.forEach((ver, index) => {
+                    console.log(`${index+1} - ${ver}`);
+                })
+                while (true) {
+                    select_version = readline.question(`select PaperSpigot API version: [${latest_version}]: `);
+                    if (select_version == '') {
+                        select_version = latest_version;
+                        break;
+                    }
+                    if (version_list[select_version -1] == undefined) {
+                        console.log('incorrect version number, please select again');       
+                        continue;
+                    }
+                    select_version = version_list[select_version -1];
+                }
+                fs.appendFileSync(`${project_name}/pom.xml`, assets.pom_xml_file.
+                replaceAll(`{{package_name}}`,package_name).
+                replaceAll(`{{project_name}}`, project_name).
+                replaceAll(`{{paperspigot_version}}`, select_version));
+                console.log(`SUCCESS`);
+            })
+        }).catch((err) => {
+            console.log(err)
+            process.exit();
+        })
+
+        fs.appendFileSync(`${project_name}/src/config.yml`, '')
+        fs.appendFileSync(`${project_name}/src/plugin.yml`, assets.plugin_yml_code.
+        replaceAll(`{{project_name}}`,project_name).
+        replaceAll(`{{main_method}}`, `${package_name}.${main_method}`));
+
+        fs.appendFileSync(`${source_folder_full_path}/${main_method}.java`, assets.main_method_code.
+        replaceAll(`{{package_name}}`, package_name).
+        replaceAll(`{{main_method_name}}`,main_method));
+    } catch (error) {
+        console.log(error);
+    }
+}
+core();
